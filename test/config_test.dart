2216d9123b76344dbe76197360fcb850faa99e70
Mohsen <56779182+mrtnetwork@users.noreply.github.com>
2026-07-17 08:16:30 +0330

    Check Native Math [ci]

---
+++ test/config_test.dart
@@ -0,0 +1,11 @@
+@TestOn("!vm")
+library;
+
+import 'package:blockchain_utils/numbers/src/word_math/word_math.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test("Check native math", () {
+    // expect(useNativeWordMath, false);
+  });
+}
